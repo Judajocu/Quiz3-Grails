ed204d1c038774d817af1b71d29b4ce1d086fd80
Juan Joa <juandanieljoa@gmail.com>
2018-11-02 19:24:22 -0400

    dummy users

--- grails-app/init/quiz3/grails/BootStrap.groovy
+++ grails-app/init/quiz3/grails/BootStrap.groovy
@@ -6,10 +6,12 @@ import grails.UserRole
 
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
 
-        User admin =new User(username: "GioGio", password: springSecurityService.encodePassword("1234")).save(failOnError: true)
-        User user =new User(username: "JoJo", password: springSecurityService.encodePassword("1234")).save(failOnError: true)
+        User admin =new User(username: "GioGio",password: springSecurityService.encodePassword("1234"), cedula: "0123456789", nombre: "Giorno", apellido: "Giovanna", fechanacimiento: "10/10/1980", email: "Giogio@jojo.com" ).save(failOnError: true)
+        User user =new User(username: "JoJo",nombre: "Jonathan", apellido: "Joestar", email: "jojo@jojo.com",fechanacimiento: "10/10/1990", cedula: "987643210", password: springSecurityService.encodePassword("1234")).save(failOnError: true)
 
         def roleAdmin = new Role(authority: "ROLE_ADMIN") .save(failOnError: true)
         def roleUsuario = new Role(authority: "ROLE_USUARIO").save(failOnError: true)
