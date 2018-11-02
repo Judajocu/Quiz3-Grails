d736e5cf6fa8ae049c493942de0e9e4f982e53ff
Juan Joa <juandanieljoa@gmail.com>
2018-11-02 18:23:45 -0400

    lol

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,39 @@
+
+//
+grails.plugin.springsecurity.password.algorithm = 'bcrypt'
+
+// Added by the Spring Security Core plugin:
+//grails.plugin.springsecurity.userLookup.userDomainClassName = 'grails.Usuario'
+//grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'grails.UsuarioRole'
+//grails.plugin.springsecurity.authority.className = 'grails.Role'
+
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        [pattern: '/',               access: ['permitAll']],
+        [pattern: '/error',          access: ['permitAll']],
+        [pattern: '/index',          access: ['permitAll']],
+        [pattern: '/index.gsp',      access: ['permitAll']],
+        [pattern: '/shutdown',       access: ['permitAll']],
+        [pattern: '/assets/**',      access: ['permitAll']],
+        [pattern: '/**/js/**',       access: ['permitAll']],
+        [pattern: '/**/css/**',      access: ['permitAll']],
+        [pattern: '/**/images/**',   access: ['permitAll']],
+        [pattern: '/**/favicon.ico', access: ['permitAll']],
+        [pattern: '/contacts/**',    access:['permitAll']],
+        [pattern: '/department/**',  access: ['permitAll']],
+        [pattern: '/usuario/**',     access: ['permitAll']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+        [pattern: '/assets/**',      filters: 'none'],
+        [pattern: '/**/js/**',       filters: 'none'],
+        [pattern: '/**/css/**',      filters: 'none'],
+        [pattern: '/**/images/**',   filters: 'none'],
+        [pattern: '/**/favicon.ico', filters: 'none'],
+        [pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
+grails.plugin.springsecurity.securityConfigType = "Annotation"
+
+//Para permitir el acceso sin la necesidad de reglas.
+grails.plugin.springsecurity.rejectIfNoRule = false
+grails.plugin.springsecurity.fii.rejectPublicInvocations = false
