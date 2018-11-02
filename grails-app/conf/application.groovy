f5714559a5bc48b84af488f6b8f1722180f276c6
Juan Joa <juandanieljoa@gmail.com>
2018-11-02 18:57:39 -0400

    security revisado, urls

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -18,9 +18,8 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
         [pattern: '/**/css/**',      access: ['permitAll']],
         [pattern: '/**/images/**',   access: ['permitAll']],
         [pattern: '/**/favicon.ico', access: ['permitAll']],
-        [pattern: '/contacts/**',    access:['permitAll']],
-        [pattern: '/department/**',  access: ['permitAll']],
-        [pattern: '/usuario/**',     access: ['permitAll']]
+        [pattern: '/eventos/**',  access: ['permitAll']],
+        [pattern: '/user/**',     access: ['permitAll']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
